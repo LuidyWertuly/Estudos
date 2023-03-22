73d746f97de1ef3e0c5ab3bcfbc8d3e5319ac8a6
Luidy Wertuly <127540434+LuidyWertuly@users.noreply.github.com>
2023-03-22 12:21:50 -0300

    Estudos 2

--- Testes programação/Python/teste4.py
+++ Testes programação/Python/teste4.py
@@ -2,9 +2,9 @@ n1 = float(input("Insira o Primeiro Numero: "))
 n2 = float(input("Insira o Segundo Numero: "))
 n3 = float(input("Insira o Terceiro Numero: "))
 
-if n1 < n2 < n3:
-    print(n1, n2, n3)
-elif n2 < n1 < n3:
-    print(n2, n3, n1)
+if n1 > n2 > n3:
+    print(n1)
+elif n2 > n1 > n3:
+    print(n2)
 else:
-    print(n3, n2, n1)
+    print(n3)
