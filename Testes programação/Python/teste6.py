1453157c79b83b1d75aeb932a78a83ae8654cd4d
Luidy Wertuly <127540434+LuidyWertuly@users.noreply.github.com>
2023-03-24 15:25:29 -0300

    versão 3

--- Testes programação/Python/teste6.py
+++ Testes programação/Python/teste6.py
@@ -4,7 +4,7 @@ areadoquadrado = lado * lado
 areadotriangulo = ((lado*lado) * 3**0.5)/4
 areatotal = areadotriangulo + areacirc + areadoquadrado
 pericirc = (((2*3.14)*lado)/2)/2
-perimrestante= 4*lado
-perimtotal= pericirc + perimrestante
-print(f'perimetro total: {perimtotal: .2f}'  )
+perimrestante = 4*lado
+perimtotal = pericirc + perimrestante
+print(f'perimetro total: {perimtotal: .2f}')
 print(f'{areatotal: .2f}')
